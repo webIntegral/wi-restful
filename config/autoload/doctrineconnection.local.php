742f72eafb6f8bf688b52789b34d15a860e74a08
Mario Orozco <mario@webintegral.com.co>
2015-10-02 20:30:24 -0500

    Add doctrine connection local configuration

---
+++ config/autoload/doctrineconnection.local.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Doctrine connection local configuration
+ * 
+ */
+
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            // default connection name
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => '',
+                    'dbname'   => 'wi',
+                    'charset' => 'utf8',
+                )
+            )
+        )
+    ),
+);
