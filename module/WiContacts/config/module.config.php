673084d4476e1f6258285341541d4b65f9ba1625
Mario Orozco <mario@webintegral.com.co>
2015-10-03 07:22:20 -0500

    Setup routes for CRUD and Doctrine support

--- module/WiContacts/config/module.config.php
+++ module/WiContacts/config/module.config.php
@@ -1,4 +1,6 @@
 <?php
+namespace WiContacts;
+
 return array(
     'controllers' => array(
         'invokables' => array(
@@ -8,35 +10,20 @@ return array(
     'router' => array(
         'routes' => array(
             'wi-contacts' => array(
-                'type'    => 'Literal',
+                'type'    => 'segment',
                 'options' => array(
                     // Change this to something specific to your module
-                    'route'    => '/contacts',
+                    'route'    => '/contacts[/][:action][/:id]',
+                    'constraints' => array(
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'        => '[0-9]+',
+                    ),
                     'defaults' => array(
                         // Change this value to reflect the namespace in which
                         // the controllers for your module are found
                         '__NAMESPACE__' => 'WiContacts\Controller',
                         'controller'    => 'Index',
                         'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // This route is a sane default when developing a module;
-                    // as you solidify the routes for your module, however,
-                    // you may want to remove it and replace it with more
-                    // specific routes.
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
                     ),
                 ),
             ),
@@ -47,4 +34,25 @@ return array(
             'WiContacts' => __DIR__ . '/../view',
         ),
     ),
+    'doctrine' => array(
+        'driver' => array(
+            // defines an annotation driver and entity paths
+            'my_annotation_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(
+                    __DIR__ . '/../src/' . __NAMESPACE__ . '/Entity',
+                ),
+            ),
+    
+            // default metadata driver, aggregates all other drivers into a single one.
+            // Override `orm_default` only if you know what you're doing
+            'orm_default' => array(
+                'drivers' => array(
+                    // register annotation driver for any entity under module namespace
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    )
 );
