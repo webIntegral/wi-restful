4e8bd6c722f60dab7ab389242f88cdab4d001ddb
Mario Orozco <mario@webintegral.com.co>
2015-10-03 20:19:35 -0500

    Create update contact form

---
+++ module/WiContact/src/WiContact/Form/UpdateContactForm.php
@@ -0,0 +1,22 @@
+<?php
+namespace WiContact\Form;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+use Zend\Form\Form;
+
+class UpdateContactForm extends Form
+{
+    public function __construct(ObjectManager $objectManager)
+    {
+        parent::__construct('update-contact-form');
+        
+        // The form will hydrate an object of type "Contact"
+        $this->setHydrator(new DoctrineHydrator($objectManager));
+        
+        // Add the fieldset and set it as base fieldset
+        $contactFieldset = new ContactFieldset($objectManager);
+        $contactFieldset->setUseAsBaseFieldset(true);
+        $this->add($contactFieldset);
+    }
+}
