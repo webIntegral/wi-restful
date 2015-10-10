8e7f3112c3de1cb1e3f8350c2d809347bf92058c
Mario Orozco <mario@webintegral.com.co>
2015-10-10 18:15:15 -0500

    zf-oauth2 configuration

---
+++ config/autoload/oauth2.local.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'zf-oauth2' => array(
+        'allow_implicit' => false, // default (set to true when you need to support browser-based or mobile apps)
+        'access_lifetime' => 3600, // default (set a value in seconds for access tokens lifetime)
+        'enforce_state'  => true,  // default
+        'storage'        => 'oauth2.doctrineadapter.default', // service name for the OAuth2 storage adapter
+    ),
+);
