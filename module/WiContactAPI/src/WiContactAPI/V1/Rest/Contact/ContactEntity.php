28680ebf6529774cdb06bf9ca99a19a4e9a070fa
Mario Orozco <mario@webintegral.com.co>
2015-10-10 15:46:45 -0500

    Define Contact Entity.

--- module/WiContactAPI/src/WiContactAPI/V1/Rest/Contact/ContactEntity.php
+++ module/WiContactAPI/src/WiContactAPI/V1/Rest/Contact/ContactEntity.php
@@ -1,6 +1,58 @@
 <?php
 namespace WiContactAPI\V1\Rest\Contact;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="WiContactAPI\V1\Rest\Contact\ContactCollection")
+ * @ORM\Table(name="contact")
+ */
 class ContactEntity
 {
+
+    /**
+     *
+     * @var int @ORM\Id
+     *      @ORM\Column(type="integer")
+     *      @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     *
+     * @var string @ORM\Column(type="string", nullable=true)
+     */
+    protected $name;
+
+    /**
+     *
+     * @return the $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     *
+     * @return the $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     *
+     * @param string $name            
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    // @todo: Add phones support
+    // @todo: Add Gedmo Annotations support
+    // @todo: Check if getArrayCopy() function is required
+
 }
