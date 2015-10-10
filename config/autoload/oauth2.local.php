fe8e36d1e8731ccd8b41e591bcca8d75d558ae25
Mario Orozco <mario@webintegral.com.co>
2015-10-10 18:20:48 -0500

    Add zf-mvc-auth configuration.

--- config/autoload/oauth2.local.php
+++ config/autoload/oauth2.local.php
@@ -1,5 +1,18 @@
 <?php
 return array(
+    'zf-mvc-auth' => array(
+        'authentication' => array(
+            'adapters' => array(
+                'oauth2_doctrine' => array(
+                    'adapter' => 'ZF\\MvcAuth\\Authentication\\OAuth2Adapter',
+                    'storage' => array(
+                        'storage' => 'oauth2.doctrineadapter.default',
+                        'route' => '/oauth',
+                    ),
+                ),
+            ),
+        ),
+    ),
     'zf-oauth2' => array(
         'allow_implicit' => false, // default (set to true when you need to support browser-based or mobile apps)
         'access_lifetime' => 3600, // default (set a value in seconds for access tokens lifetime)
