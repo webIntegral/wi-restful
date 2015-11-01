7bd2aaec6219e3c1d539d1551d82de79c1091a2b
Mario Orozco <mario@webintegral.com.co>
2015-11-01 16:58:55 -0500

    Create ContactEntity factory Creat ContactEntity factory

--- public/src/contact/grid/contactGrid.mod.js
+++ public/src/contact/grid/contactGrid.mod.js
@@ -5,50 +5,97 @@
  * 
  * Modulo para manejar el Datagrid Contactos
  */
-angular.module('wi.contact.grid', ['ui.grid', 'ui.grid.autoResize', 'ui.grid.resizeColumns', 'ui.grid.selection', 'ui.grid.pagination'])
+angular.module('wi.contact.grid', ['ngResource', 'ui.grid', 'ui.grid.autoResize', 'ui.grid.resizeColumns', 'ui.grid.selection', 'ui.grid.pagination'])
 
-.controller('ContactGridCtrl', ['$scope', 'uiGridConstants',
-function ($scope, uiGridConstants) {
+/**
+ * Contact Entity
+ */
+.factory('ContactEntity', function($resource) {
+	return $resource('/contact/:id', {
+		id: '@_id.$oid'
+	}, {
+		query: {
+			method: 'GET',
+			isArray: true,
+			transformResponse: function(data, headers) {
+				// Extraer los datos
+				var dData = angular.fromJson(data);
+				return dData._embedded.contact;
+			}
+		},
+		get: {
+			
+		},
+		update: {
+			
+		},
+		save: {
+			
+		}
+	});
+}) // ContactEntity
+
+/**
+ * Contact Grid Controller | Controlador del Grid Contact
+ */
+.controller('ContactGridCtrl', ['$scope', 'uiGridConstants', 'ContactEntity',
+function ($scope, uiGridConstants, ContactEntity) {
 	
-	$scope.myData = [
-         {
-             "firstName": "Cox",
-             "lastName": "Carney",
-             "company": "Enormo",
-             "employed": true
-         },
-         {
-             "firstName": "Lorraine",
-             "lastName": "Wise",
-             "company": "Comveyer",
-             "employed": false
-         },
-         {
-             "firstName": "Nancy",
-             "lastName": "Waters",
-             "company": "Fuelton",
-             "employed": false
-         }
-     ];
+	$scope.myData = ContactEntity.query();
+    /*ContactEntity.query().$promise.then(function (data) {
+        $scope.myData = data;
+        
+    }, function () {
+        alert('failed');
+        
+    });*/
 	
 	/*
 	 * Grid Configuration
 	 * Configuración del Grid
 	 */
 	$scope.gridOptions = {
-		
+		// Data origin
+		data: 'myData',
 		// Habilitar filtering
 		enableFiltering: true,
 		// Habilitar Multiselect
 		multiSelect: true,
 		// Pagination Config
-	    paginationPageSizes: [25, 50, 75],
-	    paginationPageSize: 25,
-	    
-		data: $scope.myData
+	    paginationPageSizes: [5, 25, 50, 75],
+	    paginationPageSize: 5,
 	}
 	
-	
-}])
+}]) // ContactGridCtrl
+
+
+
 
 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
