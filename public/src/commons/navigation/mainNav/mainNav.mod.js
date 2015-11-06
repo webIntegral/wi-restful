ebaac0879508c4edee0edc0d4cf8bad445723d18
Mario Orozco <mario@webintegral.com.co>
2015-11-06 17:25:45 -0500

    Create module and controller for main Nav.

---
+++ public/src/commons/navigation/mainNav/mainNav.mod.js
@@ -0,0 +1,11 @@
+angular.module('wi.nav.mainNav', [])
+
+/**
+ * Grid Bar Ctrlr | Controlador de la Barra del Grid
+ */
+.controller('mainNavCtrl', ['$scope',
+function ($scope) {
+
+}]) // GridBarCtrl
+
+;
