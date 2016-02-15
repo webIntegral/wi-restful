2389f89fd7bfc23308759027c1f70821f46ebc55
Mario Orozco <mario@webintegral.com.co>
2016-02-15 13:56:27 -0500

    Create router module (router provider)

--- public/src/blocks/router/router.mod.js
+++ public/src/blocks/router/router.mod.js
@@ -1,45 +1,16 @@
+/*
+ * wi.blocks.router
+ * Router Module
+ * 
+ * This module handles all application routing
+ * 
+ */
 (function() {
 	'use strict';
 	
 	angular
 		.module('wi.blocks.router', [
-             // don't we need to include ui-router dependency?
+         	'ui.router'
         ]);
 	
-	routerHelperProvider.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
-	/* @ngInject */
-	function routerHelperProvider($locationProvider, $stateProvider, $urlRouterProvider) {
-	    /* jshint validthis:true */
-	    this.$get = RouterHelper;
-
-	    $locationProvider.html5Mode(true);
-
-	    RouterHelper.$inject = ['$state'];
-	    /* @ngInject */
-	    function RouterHelper($state) {
-	        var hasOtherwise = false;
-
-	        var service = {
-	            configureStates: configureStates,
-	            getStates: getStates
-	        };
-
-	        return service;
-
-	        ///////////////
-
-	        function configureStates(states, otherwisePath) {
-	            states.forEach(function(state) {
-	                $stateProvider.state(state.state, state.config);
-	            });
-	            if (otherwisePath && !hasOtherwise) {
-	                hasOtherwise = true;
-	                $urlRouterProvider.otherwise(otherwisePath);
-	            }
-	        }
-
-	        function getStates() { return $state.get(); }
-	    }
-	}
-		
 })();
