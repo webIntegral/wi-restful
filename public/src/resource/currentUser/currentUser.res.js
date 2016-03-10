cfc4020a6fc53ac10e47309b29b215933f40d8a8
Mario Orozco <mario@webintegral.com.co>
2016-03-10 13:41:03 -0500

    Create Current User Resource to retrieve authenticated User

---
+++ public/src/resource/currentUser/currentUser.res.js
@@ -0,0 +1,28 @@
+(function() {
+	'use strict';
+	
+	/**
+	 * Current User Resource
+	 * Resource to get current user from server.
+	 * Server uses Satellizer's Acces Token to find user.
+	 */
+	
+	angular
+		.module('wi.resource.currentUser')
+		.factory('CurrentUserResource', CurrentUserResource);
+	
+	CurrentUserResource.$inject = ['$resource', '$auth'];
+		
+	function CurrentUserResource($resource, $auth) {
+		return $resource('/user-by-token', {
+			
+		}, {
+			get: {
+				transformResponse: function(data, headers) {
+					var dData = angular.fromJson(data);
+					return dData;
+				}
+			}
+		});
+	} //CurrentUserResource
+})();
