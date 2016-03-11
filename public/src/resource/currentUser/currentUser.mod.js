7d6193fa500858f2f50ce3b0db5c4a061541c3f8
Mario Orozco <mario@webintegral.com.co>
2016-03-11 08:55:21 -0500

    Document

--- public/src/resource/currentUser/currentUser.mod.js
+++ public/src/resource/currentUser/currentUser.mod.js
@@ -1,9 +1,17 @@
+/**
+ * wi.resource.currentUser
+ * 
+ * Module Current User Resource
+ * 
+ * Resource to get current user from server.
+ * Server uses Satellizer's Acces Token to find user.
+ * 
+ */
 (function() {
 	'use strict';
 	
 	angular
 		.module('wi.resource.currentUser', [
-            'wi.oauth',
-		    'ngResource'
+            'ngResource'
         ])
 })();
--- public/src/resource/currentUser/currentUser.res.js
+++ public/src/resource/currentUser/currentUser.res.js
@@ -1,11 +1,14 @@
+/**
+ * wi.resource.currentUser
+ * 
+ * Current User Resource
+ * 
+ * Resource to get current user from server.
+ * Server uses Satellizer's Acces Token to find user.
+ * 
+ */
 (function() {
 	'use strict';
-	
-	/**
-	 * Current User Resource
-	 * Resource to get current user from server.
-	 * Server uses Satellizer's Acces Token to find user.
-	 */
 	
 	angular
 		.module('wi.resource.currentUser')
