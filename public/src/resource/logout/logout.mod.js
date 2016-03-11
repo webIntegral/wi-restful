d4dbd3d847fd0f0b3eb71ce06f207519f03d1772
Mario Orozco <mario@webintegral.com.co>
2016-03-11 08:57:52 -0500

    Document

--- public/src/resource/logout/logout.mod.js
+++ public/src/resource/logout/logout.mod.js
@@ -1,9 +1,17 @@
+/**
+ * wi.resource.logout
+ * 
+ * Logout Resource
+ * 
+ * This Resource requests the server to delete token to "kill session".
+ * Server uses Satellizer Token.
+ * 
+ */
 (function() {
 	'use strict';
 	
 	angular
 		.module('wi.resource.logout', [
-           'wi.oauth',
 		   'ngResource'
         ]);
 })();
--- public/src/resource/logout/logout.res.js
+++ public/src/resource/logout/logout.res.js
@@ -1,3 +1,12 @@
+/**
+ * wi.resource.logout
+ * 
+ * Logout Resource
+ * 
+ * This Resource requests the server to delete token to "kill session".
+ * Server uses Satellizer Token.
+ * 
+ */
 (function() {
 	'use strict';
 	
